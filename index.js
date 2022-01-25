ba4ec44cff69f2cbd31b5d344e580e7001d147e4
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-01-24 22:40:39 -0500

    add notification bar

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-$("head").append(
-  '<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">'
+const notificationBarElement = document.getElementById('notification-bar');
+const notificacionBarButton = document.getElementById(
+  'notification-bar--close-button',
 );
+
+console.log(notificacionBarButton);
+
+notificacionBarButton.addEventListener('click', () => {
+  // handleEvent: closeNotificationBar
+  closeNotificationBar();
+});
+
+function closeNotificationBar() {
+  notificationBarElement.style.display = 'none';
+}
+
+// $('head').append(
+//   '<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">',
+// );
